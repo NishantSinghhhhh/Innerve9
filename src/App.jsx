14809afe261248ca81b4f4aa3e6ce37a1a9768c8
Mohit <me.mohitsharma14@gmail.com>
2024-11-17 22:32:06 +0530

    Prize Section Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import "./App.css";
+
+import Cards from "./components/Cards/index"
 
 function App() {
 
 
   return (
     <>
-        <h1 className="text-3xl font-bold underline text-red-400">Hello world!</h1>
-        
+      <Cards/>
     </>
   );
 }
---
+++ src/components/Cards/index.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import CardWrapper from "./CardWrapper";
+import radialfirst from "../../assets/radialtrianglefirst.svg";
+import radialsecond from "../../assets/radialtrianglesecond.svg"; 
+import radialthird from "../../assets/radialtrianglethird.svg";  
+import leftside from "../../assets/leftsidecut.svg";
+import rightside from "../../assets/rightsidecut.svg";
+import egg1 from "../../assets/Egg1-basket.svg"
+import egg2 from "../../assets/Egg2-basket.svg"
+import egg3 from "../../assets/Egg3-basket.svg"
+import BackgroundPrize from "../../assets/./BackgroundPrize.svg";
+import Cloud from "../../assets/./Cloudsbackground.svg";
+import { div } from "three/webgpu";
+const Cards = () => {
+  const cardProps = {
+    cardProps: {
+      
+      secondPrize: {
+        prize: "Second Prize",
+        prizeLabel: "SECOND",
+        amount: "50K+",
+        description:
+          "Innerve Goodies and Merch. Fundings and Internship opportunities from our sponsors",
+        images: {radial:radialsecond  , leftside, rightside,egg:egg3 },
+      },
+      firstPrize: {
+        prize: "First Prize",
+        prizeLabel: "FIRST",
+        amount: "100K+",
+        description:
+          "Innerve Goodies and Merch. Fundings and Internship opportunities from our sponsors",
+        images: { radial:radialfirst, leftside, rightside, egg:egg3 },
+      },
+      thirdPrize: {
+        prize: "Third Prize",
+        prizeLabel: "THIRD",
+        amount: "25K+",
+        description:
+          "Innerve Goodies and Merch. Fundings and Internship opportunities from our sponsors",
+        images: { radial: radialthird, leftside, rightside,egg:egg3 },
+      },
+    },
+  };
+
+  return (
+    <div className="absolute top-[0%] z-[-2] w-screen transform  bg-black">
+        <p className="absolute text-[60px] top-[30%] z-[5] text-white left-[50%] transform -translate-x-1/2">PRIZE POOL OF Rs. 10,00,000+</p>
+    <div className="absolute flex justify-center items-center top-[30%] w-screen h-screen  gap-5 z-[5] ">
+             
+      <CardWrapper {...cardProps.cardProps.secondPrize} />
+      <CardWrapper {...cardProps.cardProps.firstPrize} />
+      <CardWrapper {...cardProps.cardProps.thirdPrize} />
+    </div>
+    <img src={Cloud} alt="Cloud Background" className="relative z-[-4]"/>
+    <img src={BackgroundPrize} alt="Background" className="absolute top-[12%] z-[4]" />
+
+    </div>
+  );
+};
+
+export default Cards;
