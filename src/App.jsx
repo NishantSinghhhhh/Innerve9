d1abfa2747aabaf4598d7ef7bd2c208e83cd2a60
ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>
2024-11-19 21:59:27 +0530

    fixed(ui):width/font

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-
-import React from 'react';
-import Tracks from './pages/Tracks';  
-import Hero from "./components/Hero"
+import Tracks from "./pages/Tracks";
+import Hero from "./components/Hero";
 import "./App.css";
-import Cards from "./components/Cards/index"
+import Cards from "./components/Cards/index";
 
 function App() {
   return (
     <>
-      <Hero /> 
-      <Cards/>
-      <div className="app-container">
-        <Tracks />  
-      </div>
+      <Hero />
+      <Cards />
+      <Tracks />
     </>
   );
-};
+}
 
 export default App;
