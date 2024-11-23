5ee478d71e70f0b6a8db2908610d62a4641d3737
ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>
2024-11-24 02:01:44 +0530

    timeline animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,13 +11,20 @@ const Navbar = () => {
         </a>
         <div className="flex gap-4 items-center">
           <div className="flex items-center gap-2 cursor-pointer px-4 group">
-            <div className="bg-[#3AFFA3] h-[20px] w-1 shadow-[0_0_36px_8px_rgba(58,255,163,0.5)] transition-shadow duration-100 group-hover:shadow-none"></div>
+            <span className="h-[20px] w-1 bg-[#3AFFA3] z-10"></span>
+            <span
+              className="h-3 w-1 bg-[#3AFFA3] absolute -translate-x-1/2"
+              style={{
+                animation: "growBlur 500ms infinite",
+                opacity: 1,
+              }}
+            ></span>
             <p>Brochure</p>
             <img src={Navigate} alt="brochure" />
           </div>
           <button className="flex items-center gap-[10px] border border-[rgba(255,255,255,0.5)] pl-6 pr-4 py-[10px] rounded">
-            <p>Merit</p>
-            <img src={Navigate} alt="merit" />
+            <p>Menu</p>
+            <img src={Navigate} alt="menu" />
           </button>
         </div>
       </nav>
--- src/components/TimelineDetails.jsx
+++ src/components/TimelineDetails.jsx
@@ -6,23 +6,23 @@ import Black from '../assets/Dot.svg';
 // eslint-disable-next-line react/prop-types
 const TimelineDetails = ({ title, date, description }) => {
   return (
-    <div className="text-black flex gap-[2rem] flex-row">
+    <div className="text-black flex gap-6 md:gap-[44px] flex-row">
       {/* Dots */}
       <div className="flex flex-col gap-[1rem] justify-center items-center h-full">
-        <img src={White} className="w-[12px] h-[12px]" alt="White Dot" />
         <img src={Black} className="w-[40px] h-[40px]" alt="Black Dot" />
+        <img src={White} className="w-[12px] h-[12px]" alt="White Dot" />
         <img src={White} className="w-[12px] h-[12px]" alt="White Dot" />
         <img src={White} className="w-[12px] h-[12px]" alt="White Dot" />
         <img src={White} className="w-[12px] h-[12px]" alt="White Dot" />
       </div>
 
       {/* Details */}
-      <div className="w-[70%]">
-        <div className="flex flex-row justify-between">
-          <p className="font-angrybirds text-[20px]">{title}</p>
+      <div className="w-[80%]">
+        <div className="flex flex-row justify-between items-center">
+          <p className="font-angrybirds text-base md:text-[20px]">{title}</p>
           <p className="font-tthoves text-[14px] text-gray-500">{date}</p>
         </div>
-        <div className="font-tthoves text-left text-[16px] pt-[1rem]">{description}</div>
+        <div className="font-tthoves text-left text-sm md:text-base pt-[1rem]">{description}</div>
       </div>
     </div>
   );
