bb12824a4120eedb6fb52315eac524103b3fbc85
jyotimishra6789 <riamishra639@gmail.com>
2024-12-12 13:30:15 +0530

    Adding tracks section
    Signed-off-by: jyotimishra6789 <riamishra639@gmail.com>

--- src/pages/Tracks.jsx
+++ src/pages/Tracks.jsx
@@ -2,30 +2,42 @@ import React from "react";
 import "./Tracks.css";
 
 import HeartImg from "../assets/heart.svg"; 
+import BlockchainImg from "../assets/Blockchain.svg"; 
+import LogiImg from "../assets/LOGISTICS.svg";
+import FinImg from "../assets/fintech.svg"; 
+import EdImg from "../assets/EDUTECH.svg"; 
+import VrImg from "../assets/vr.svg"; 
+import DefImg from "../assets/defence.svg"; 
+import GameImg from "../assets/game.svg"; 
+import SecImg from "../assets/security.svg"; 
 import RaysImg1 from "../assets/rays.svg"; 
 import RaysImg2 from "../assets/rays2.svg"; 
 import RaysImg3 from "../assets/rays3.svg"; 
-
 
 const innerCardColors = [
   "#FA86B4", "#549EDE", "#903CD2", "#903CD2", "#FA86B4", 
   "#549EDE", "#549EDE", "#903CD2", "#FA86B4"
 ];
 
-
 const raysImages = [
   RaysImg1, RaysImg2, RaysImg3, RaysImg3, RaysImg1, 
   RaysImg2, RaysImg2, RaysImg3, RaysImg1
 ];
+
+const outerImages = [
+  VrImg, EdImg, HeartImg, GameImg, FinImg, 
+  SecImg, DefImg, BlockchainImg, LogiImg
+];
+
 const labels = [
   "AR/VR", "EdTech", "Health", "Gaming", "FinTech", 
   "CyberSec", "Defence", "Blockchain", "Logistics"
 ];
 
-const Card = ({ innerColor, raysImage }) => {
+const Card = ({ innerColor, raysImage, outerImage }) => {
   return (
     <div className="outer-card">
-      <img src={HeartImg} alt="Heart" className="heart-image" /> 
+      <img src={outerImage} alt="Outer Card" className="outer-image" /> 
       <div className="inner-card" style={{ backgroundColor: innerColor }}>
         <img src={raysImage} alt="Rays" className="inner-image" />
       </div>
@@ -43,6 +55,7 @@ const Tracks = () => {
             <Card 
               innerColor={innerColor} 
               raysImage={raysImages[index]} 
+              outerImage={outerImages[index]} 
             />
             <div className="card-label-container">
               <h2 className="card-label">{labels[index]}</h2> 
