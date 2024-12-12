124029859514368b671a94777e258a2389d0335a
Mohit <me.mohitsharma14@gmail.com>
2024-12-12 18:45:28 +0530

    Sponsors images added, reduced size compressed and sponsors tab improved

--- src/components/SponsorBox.jsx
+++ src/components/SponsorBox.jsx
@@ -9,7 +9,7 @@ const SponsorBox = ({ image}) => {
       
       <div className="relative md:w-[23rem] w-[20rem] h-[6.25rem] border-8 border-[#734F1F] transform -skew-x-12 flex items-center justify-center">
         <div className="flex items-center justify-center bg-white border-4 border-[#FFC102] transform -skew-x-10 h-full w-full">
-          <img className="h-[3rem] w-[10rem]" src={image} alt="Sponsor Logo" />
+          <img className="h-[3rem] w-[10rem] transform skew-x-12" src={image} alt="Sponsor Logo" />
         </div>
       </div>
     </div>
--- src/pages/Sponsors.jsx
+++ src/pages/Sponsors.jsx
@@ -7,11 +7,25 @@ import UpperLeaves from "../assets/Orange_Leaves_1.png";
 import LowerLeaves from "../assets/Orange_Leaves_bottom.png";
 import Texture from "../assets/texture.svg";
 import SponsorTab from '../components/SponsorTab';
+import solana from "../assets/solana.svg";
+import microsoft from "../assets/Microsoft.svg"
+import kaagaz from "../assets/kaagaz.svg"
+import quine from "../assets/quine.svg"
+import mro from "../assets/mro.svg"
+import tailcall from "../assets/taicall.svg"
+import replit from "../assets/replit.svg"
+import echo from "../assets/echo.svg"
+import xyz from "../assets/xyz.svg"
+import verbwire from "../assets/verbwire.svg"
+import icici from "../assets/icici.svg"
 
 const Sponsors = () => {
-  const titleSponsorImages = [Polygom, Devfolio, EthIndia];
-  const goldSponsorImages = [Accops, Accops, Accops];
-  const silverSponsorImages = [Accops, Accops, Accops];
+  const titleSponsorImages = [Accops, mro];
+  const platinumSponsorImages = [tailcall, microsoft];
+  const platformSponsorImages = [solana, Devfolio];
+  const coSponsorImages = [kaagaz,replit, quine];
+  const subSponsorImages = [verbwire, xyz,echo];
+  const goodwillPartnerImages = [icici];
 
   return (
     <section className="relative w-full flex flex-col justify-center items-center">
@@ -29,18 +43,20 @@ const Sponsors = () => {
       <div className='absolute w-full h-full top-0 left-0' style={{backgroundImage: `url(${Texture})`, backgroundPosition:"top", backgroundRepeat:"repeat", backgroundSize:"100%", mixBlendMode:"soft-light", opacity:0.8}} ></div>
         {/* heading */}
         <div className='flex flex-col md:flex-row gap-0'>
-          <p className='capitalize text-[32px] md:text-7xl font-angrybirds text-[#FFFF5C] leading-none'>Our</p>
+          <p className='capitalize text-[32px] md:text-7xl font-angrybirds text-[#FFFF5C] leading-none'>Our Past</p>
           <p className='capitalize text-[80px] md:text-[128px] font-angrybirds text-[#FFFF5C] leading-none'>Sponsors</p>
         </div>
 
         {/* sponsors tab */}
         <div className='px-6'>
-            <SponsorTab heading="Title Sponsor" images={titleSponsorImages}  piggyNumber={0}/>
-            <SponsorTab heading="Gold Sponsors" images={goldSponsorImages} piggyNumber={1}/>
-            <SponsorTab heading="Silver Sponsors" images={silverSponsorImages}  piggyNumber={2}/>
-            <SponsorTab heading="Title Sponsor" images={silverSponsorImages}  piggyNumber={0}/>
-            <SponsorTab heading="Gold Sponsors" images={goldSponsorImages} piggyNumber={1}/>
-            <SponsorTab heading="Silver Sponsors" images={silverSponsorImages} piggyNumber={2}/>
+            
+        <SponsorTab heading="Title Sponsor" images={titleSponsorImages}  piggyNumber={0}/>
+        <SponsorTab heading="Platinum Sponsor" images={platinumSponsorImages}  piggyNumber={0}/>
+            <SponsorTab heading="Platform Sponsors" images={platformSponsorImages} piggyNumber={1}/>
+            <SponsorTab heading="Co Sponsors" images={coSponsorImages}  piggyNumber={2}/>
+            <SponsorTab heading="Sub Sponsor" images={subSponsorImages}  piggyNumber={0}/>
+            <SponsorTab heading="GoodWill Partner" images={goodwillPartnerImages} piggyNumber={1}/>
+            {/* <SponsorTab heading="Silver Sponsors" images={silverSponsorImages} piggyNumber={2}/> */}
         </div>
       </div>
 
