14f9755715d0e156c960baa7ee8e30779c7d0992
NishantSinghhhhh <nishantsingh_230137@aitpune.edu.in>
2024-12-17 01:05:22 +0530

    Made footer better
    Signed-off-by: NishantSinghhhhh <nishantsingh_230137@aitpune.edu.in>

--- src/pages/Footer2.jsx
+++ src/pages/Footer2.jsx
@@ -5,44 +5,101 @@ import gram from "../assets/gram.svg";
 import ins from "../assets/ins.svg";
 import twir from "../assets/twir.svg";
 import lock from "../assets/lock.svg";
-import mapFrameImage from "../assets/card.png";
+import BlinkingDot from "../components/blinking.jsx";
 import "./Footer2.css";
+import mapFrameImage from "../assets/card.png";
 
 const Footer2 = () => {
+  const planets = [
+    { id: 1, img: lock, alt: "Lock Icon" },
+    { id: 2, img: gram, alt: "Gram Icon" },
+    { id: 3, img: lock, alt: "Lock Icon" },
+    { id: 4, img: twir, alt: "Twitter Icon" },
+    { id: 5, img: cord, alt: "Cord Icon" },
+    { id: 6, img: lock, alt: "Lock Icon" },
+    { id: 7, img: ins, alt: "Instagram Icon" },
+    { id: 8, img: lock, alt: "Lock Icon" },
+    { id: 9, img: lock, alt: "Lock Icon" },
+    { id: 10, img: lock, alt: "Lock Icon" },
+  ];
+
   return (
-    <div className="all">
-    <div className="solar-system">
-    <div className="sun"><img src={creamRoth} alt="" />
-    <div className="board">CONNECT WITH US!</div>
-    </div>
-    <div className="planet planet1"><img src={lock} alt="" /></div>
-    <div className="planet planet10"><img src={lock} alt="" /></div>
-    <div className="planet planet2"><img src={gram} alt="" /></div>
-    <div className="planet planet3"><img src={lock} alt="" /></div>
-    <div className="planet planet4"><img src={twir} alt="" /></div>
-    <div className="planet planet5"><img src={cord} alt="" /></div>
-    <div className="planet planet6"><img src={lock} alt="" /></div>
-    <div className="planet planet7"><img src={ins} alt="" /></div>
-    <div className="planet planet8"><img src={lock} alt="" /></div>
-    <div className="planet planet9"><img src={lock} alt="" /></div>
-  </div>
-  <div className="contact">
-    <div className="sec1"></div>
-    <div className="sec2"></div>
-    <div className="map"> 
+    <div className="footer-container">
+      <div className="box">
+
+      <div>
+        
+      </div>
+      {/* Solar System Section */}
+      <div className="solar-system">
+        <div className="sun">
+          <img src={creamRoth} alt="Cream Roth Logo" />
+          <div className="board">CONNECT WITH US!</div>
+        </div>
+        {planets.map((planet, index) => (
+          <div key={index} className={`planet planet${index + 1}`}>
+            <img src={planet.img} alt={planet.alt} />
+          </div>
+        ))}
+      </div>
+
+      {/* Contact Section */}
+      <div className="xl:w-1/4 flex flex-col items-center justify-center mt-4">
+      <h3 className="text-xl font-semibold mb-2">Venue</h3>
+      <div className="relative w-[280px] h-[200px] mt-2 flex items-center justify-center">
+        <img
+          src={mapFrameImage}
+          alt="Map Frame"
+          className="absolute w-full h-full object-contain"
+          />
         <iframe
           title="Venue Location"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3781.2698970162255!2d73.87251157543034!3d18.606926382503463!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c710e77af169%3A0x585dffe6ae2cf0f0!2s20%2C%20St%20Dnyaneshwar%20Rd%2C%20Runwal%20Park%2C%20Vijay%20Nagar%2C%20Dighi%2C%20Pimpri-Chinchwad%2C%20Maharashtra%20411015!5e0!3m2!1sen!2sin!4v1732267644510!5m2!1sen!2sin"
           width="180"
           height="180"
-          className=""
+          className="absolute top-[10px] left-[48px] right-[10px] bottom-[10px] border-none rounded-xl"
           allowFullScreen=""
           loading="fast"
-          ></iframe></div>
-  </div>
-  </div>
-);
-};
+          ></iframe>
+      </div>
+    </div>
 
+      </div>
+
+        <div>
+        <div className="border-t border-[#9CA3AF] pt-[10px] w-full min-h-14 
+      flex flex-col 
+      md:flex-row 
+      md:items-center 
+      md:justify-center">
+      <div className="w-[95%] 
+        flex flex-col 
+        md:flex-row 
+        items-center 
+        justify-between 
+        space-y-4 
+        md:space-y-0">
+        <div className="flex items-center gap-[10px]">
+          <BlinkingDot />
+        </div>
+        
+        <div className="text-[#9CA3AF] text-[15px] md:text-[18px] text-center">
+          Made with ❤️ by Open Source Software Club
+        </div>
+        
+        <div className="flex gap-[15px]">
+          {/* <img src={discord1} alt="Discord" className="w-6 h-6 md:w-auto md:h-auto" />
+          <img src={github} alt="GitHub" className="w-6 h-6 md:w-auto md:h-auto" />
+          <img src={meta} alt="Meta" className="w-6 h-6 md:w-auto md:h-auto" />
+          <img src={X_1} alt="X" className="w-6 h-6 md:w-auto md:h-auto" />
+          <img src={linkedIn} alt="LinkedIn" className="w-6 h-6 md:w-auto md:h-auto" /> */}
+        </div>
+      </div>
+    </div>
+        </div>
+      
+    </div>
+  );
+};
 
 export default Footer2;
