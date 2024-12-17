696f5b1a0024633a180a5c67990e9ecf281a9171
Pavankumar07s <pawankumar14662693@gmail.com>
2024-12-17 21:44:03 +0530

    sponsors page done

--- src/components/SponsorTab.jsx
+++ src/components/SponsorTab.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
-import SponsorBox from './SponsorBox';
-import PiggySponsorBox from './PiggySponsorBox';
+import PropTypes from 'prop-types';
 
 const SponsorTab = ({ heading, images, piggyNumber }) => {
-  const sponsorBoxes = images.map((image, index) => {
-    if (index === piggyNumber) {
-      return <PiggySponsorBox key={index} image={image} />;
-    }
-    return <SponsorBox key={index} image={image} />;
-  });
-
   return (
-    <div className="pb-[5rem]">
-      <p className="font-angrybirds text-[36px] text-[#FFFF5C] pb-[3rem]">{heading}</p>
-      <div className="flex-wrap gap-[4rem] mt-4 flex flex-row items-center justify-center">
-        {sponsorBoxes}
+    <div className="w-full">
+      <div className="flex flex-col items-center">
+        <h3 className="text-2xl md:text-4xl font-angrybirds text-white mb-6 md:mb-8">
+          {heading}
+        </h3>
+        <div className="flex flex-wrap justify-center gap-8 md:gap-12">
+          {images.map((Image, index) => (
+            <div 
+              key={index} 
+              className="w-32 h-32 md:w-40 md:h-40 bg-white rounded-2xl flex items-center justify-center p-4"
+            >
+              {Image}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
+};
+
+SponsorTab.propTypes = {
+  heading: PropTypes.string.isRequired,
+  images: PropTypes.arrayOf(PropTypes.node).isRequired,
+  piggyNumber: PropTypes.number.isRequired,
 };
 
 export default SponsorTab;
